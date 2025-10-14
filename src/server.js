37d96cec08c1457fa1e61783152b49639e4d767d
Mr.Igor <iv_mirsoft@ukr.net>
2025-10-14 22:04:53 +0300

    dz4:00005 server/error fixes

--- src/server.js
+++ src/server.js
@@ -13,14 +13,16 @@ import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js'
 
 const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+//
+//const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+
 app.use(logger);
-
-const PORT = process.env.PORT ?? 3030;
-
 app.use(authRoutes);
 app.use(notesRoutes);
 
