a73cfb5e71d5893f6320553297f9d4af85809ecb
Mr.Igor <iv_mirsoft@ukr.net>
2025-10-25 10:08:25 +0300

    user profile route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { updateUserAvatar } from "../controllers/userController.js";
+import { getUser, updateUserAvatar } from "../controllers/userController.js";
 import { upload } from "../middleware/multer.js";
 import { authenticate } from "../middleware/authenticate.js";
 
@@ -7,5 +7,6 @@ import { authenticate } from "../middleware/authenticate.js";
 const router = Router();
 
 router.patch("/users/me/avatar", authenticate, upload.single("avatar"), updateUserAvatar)
+router.get("/users/me", authenticate, getUser)
 
 export default router
