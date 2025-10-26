c7ee2b4d816d5b584350f8e10e7038ac0be1d6b0
Mr.Igor <iv_mirsoft@ukr.net>
2025-10-26 18:54:53 +0200

    -api fixes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -18,8 +18,8 @@ import {
 
 const router = Router();
 
-router.post('api/auth/register', celebrate(registerUserSchema), registerUser)
-router.post('api/auth/login', celebrate(loginUserSchema), loginUser)
+router.post('/api/auth/register', celebrate(registerUserSchema), registerUser)
+router.post('/api/auth/login', celebrate(loginUserSchema), loginUser)
 router.post('/auth/logout', logoutUser)
 router.post('/auth/refresh', refreshUserSession)
 
