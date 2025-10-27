8761f5a706be337dedbe941c2aa3c03a18ae95d3
Mr.Igor <iv_mirsoft@ukr.net>
2025-10-27 07:16:31 +0200

    log details

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,15 +1,21 @@
 import pino from 'pino-http';
 
 export const logger = pino({
-	level: 'info',
+	level: "info",
+	customProps: (req, res) => {
+		return {
+			ip: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+			user: req.user?.username || "guest",
+		}
+	},
 	transport: {
-		target: 'pino-pretty',
+		target: "pino-pretty",
 		options: {
 			colorize: true,
-			translateTime: 'HH:MM:ss',
-			ignore: 'pid,hostname',
-			messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms {req.user.username}',
+			translateTime: "HH:MM:ss",
+			ignore: "pid,hostname",
+			messageFormat: '{ip} {user} : {req.method} {req.url} {res.statusCode} - {responseTime}ms',
 			hideObject: true,
 		},
 	},
-});
+})
